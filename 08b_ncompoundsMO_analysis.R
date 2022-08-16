ed34a8db3374d8df1fecd68cb5e1f8bddddfdf93
Stephanie Cunningham <stcunnin@esf.edu>
2022-08-16 11:54:17 -0400

    Wrote averaged coefficients to file

--- 08b_ncompoundsMO_analysis.R
+++ 08b_ncompoundsMO_analysis.R
@@ -447,7 +447,16 @@ for (i in 1:10) {
 }
 
 # Calculate averages for each coefficient
-coef_avg <- colMeans(m_est[sapply(m_est, is.numeric)])
-pct2.5_avg <- colMeans(pct2.5[sapply(pct2.5, is.numeric)])
-pct97.5_avg <- colMeans(pct97.5[sapply(pct97.5, is.numeric)])
+coef_avg <- colMeans(m_est[sapply(m_est, is.numeric)], na.rm=TRUE)
+pct2.5_avg <- colMeans(pct2.5[sapply(pct2.5, is.numeric)], na.rm=TRUE)
+pct97.5_avg <- colMeans(pct97.5[sapply(pct97.5, is.numeric)], na.rm=TRUE)
+
+coef_summary <- bind_rows(coef_avg, pct2.5_avg, pct97.5_avg)
+coef_summary <- as.data.frame(coef_summary)
+coefs <- c("coef_mean", "2.5CI", "97.5CI")
+coef_summary <- data.frame(coef=coefs, coef_summary)
+
+# Write to file
+write_csv(coef_summary, "results/ncompMO_coef-summary.csv")
+
 
