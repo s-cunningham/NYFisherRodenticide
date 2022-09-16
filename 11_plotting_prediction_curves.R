27bef58539a2d4e6dc3fddf63e8353ba1a1e8d8a
Stephanie Cunningham <stcunnin@esf.edu>
2022-09-16 12:21:51 -0400

    Reading in data

--- 11_plotting_prediction_curves.R
+++ 11_plotting_prediction_curves.R
@@ -4,4 +4,22 @@
 library(tidyverse)
 library(patchwork)
 
+#### Number of compounds ####
+dat <- read_csv("output/model_data.csv")
+
+
+
+
+#### Age-specific Number of Compounds ####
+
+
+
+
+#### Binary analyses #####
+dat2 <- read_csv("output/binary_model_data.csv")
+
+# Subset by compound
+brod <- dat2[dat2$compound=="Brodifacoum",]
+brom <- dat2[dat2$compound=="Bromadiolone",]
+diph <- dat2[dat2$compound=="Diphacinone",]
 
