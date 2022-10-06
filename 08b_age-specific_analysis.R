78800c3c843c6f09e117d838e1c6fc0240dac9f8
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2022-10-06 15:23:18 -0300

    Tidying covariates

--- 08b_age-specific_analysis.R
+++ 08b_age-specific_analysis.R
@@ -3,24 +3,13 @@ library(MuMIn)
 library(ordinal)
 
 options(scipen=999, digits=3)
-set.seed(123)
+set.seed(1)
 
 #### Read in data ####
 dat <- read_csv("data/analysis-ready/combined_AR_covars.csv")
 dat <- as.data.frame(dat)
 
 #### Analysis Set-up ####
-
-# Categorical number of compounds (collapsing higher numbers)
-dat$catNcompMO <- ifelse(dat$n.compounds.MO>=2, "2+", as.character(dat$n.compounds.MO))
-dat$catNcompMO <- ordered(dat$catNcompMO, levels=c("0", "1", "2+"))
-
-# Order response
-dat$n.compounds.MO <- ordered(dat$n.compounds.MO, levels=c(0,1,2,3))
-
-# Categorical number of compounds (collapsing higher numbers)
-dat$catNcompT <- ifelse(dat$n.compounds.T>=3, "3+", as.character(dat$n.compounds.T))
-dat$catNcompT  <- ordered(dat$catNcompT , levels=c("0", "1", "2", "3+"))
 
 # Make random effects factors
 dat$WMU <- as.factor(dat$WMU)
@@ -43,62 +32,46 @@ dat[,c(10,17:24)] <- scale(dat[,c(10,17:24)])
 subads <- dat %>% filter(catAge=="subadult")
 
 ## Percent AG
-pctAG1 <- subads[, c(1:15, 17:19)]
-pctAG1 <- distinct(pctAG1)
-pctAG1 <- pctAG1 %>% group_by(RegionalID) %>% 
-  pivot_wider(names_from=buffsize, values_from=c(pasture, crops, totalag)) %>% as.data.frame()
+pctAG <- dat %>% select(RegionalID, pt_name, pt_index, buffsize, pasture, crops, totalag) %>% 
+  distinct() %>% 
+  group_by(RegionalID) %>% 
+  pivot_wider(names_from=buffsize, values_from=c(pasture, crops, totalag)) %>% 
+  as.data.frame()
 
 ## Beech basal area
-baa1 <- subads[, c(1:15,23,24)]
-baa1  <- baa1  %>% group_by(RegionalID) %>% pivot_wider(names_from=buffsize, values_from=c(BMI, laggedBMI), values_fn=unique) %>% as.data.frame()
+baa1 <- dat %>% select(RegionalID, pt_name, pt_index, buffsize, BMI, laggedBMI) %>% 
+  distinct() %>% 
+  group_by(RegionalID) %>% 
+  pivot_wider(names_from=buffsize, values_from=c(BMI, laggedBMI), values_fn=unique) %>% 
+  as.data.frame()
 
 ## Wildland-urban interface
-intermix1 <- subads[, c(1:16, 20)] 
-intermix1 <- intermix1 %>% unite("buffrad", 15:16, sep="_") %>% 
+intermix1 <- dat %>% select(RegionalID, pt_name, pt_index, buffsize, radius, intermix) %>%
+  unite("buffrad", 4:5, sep="_") %>% 
   group_by(RegionalID) %>% 
-  pivot_wider(names_from=buffrad, values_from=intermix) %>% as.data.frame()
-names(intermix1)[15:23] <- c("mix_15_100", "mix_30_100", "mix_60_100",
-                             "mix_15_250", "mix_30_250", "mix_60_250",
-                             "mix_15_500", "mix_30_500", "mix_60_500") 
+  pivot_wider(names_from=buffrad, values_from=intermix) %>% 
+  as.data.frame()
+names(intermix1)[4:12] <- c("mix_15_100", "mix_30_100", "mix_60_100",
+                            "mix_15_250", "mix_30_250", "mix_60_250",
+                            "mix_15_500", "mix_30_500", "mix_60_500") 
 
-interface1 <- dat[, c(1:16, 21)]
-interface1 <- interface1  %>% unite("buffrad", 15:16, sep="_") %>%
-  group_by(RegionalID) %>% pivot_wider(names_from=buffrad, values_from=interface) %>% as.data.frame()
-names(interface1)[15:23] <- c("face_15_100", "face_30_100", "face_60_100",
-                              "face_15_250", "face_30_250", "face_60_250",
-                              "face_15_500", "face_30_500", "face_60_500") 
-
-wui1 <- dat[, c(1:16, 22)]
-wui1 <- wui1 %>% unite("buffrad", 15:16, sep="_") %>%
+interface1  <- dat %>% select(RegionalID, pt_name, pt_index, buffsize, radius, interface) %>%
+  unite("buffrad", 4:5, sep="_") %>% 
   group_by(RegionalID) %>% 
-  pivot_wider(names_from=buffrad, values_from=totalWUI) %>% as.data.frame()
-names(wui1)[15:23] <- c("wui_15_100", "wui_30k_100", "wui_60_100",
-                        "wui_15_250", "wui_30k_250", "wui_60_250",
-                        "wui_15_500", "wui_30k_500", "wui_60_500") 
+  pivot_wider(names_from=buffrad, values_from=interface) %>% 
+  as.data.frame()
+names(interface1)[4:12] <- c("face_15_100", "face_30_100", "face_60_100",
+                             "face_15_250", "face_30_250", "face_60_250",
+                             "face_15_500", "face_30_500", "face_60_500") 
 
-# Join intermix WUI
-intermix1 <- intermix1[,c(1:3, 14:22)]
-wui1 <- left_join(wui1, intermix1, by=c("RegionalID", "pt_name", "pt_index"))
-
-# Join interface WUI
-interface1 <- interface1[, c(1:3, 14:22)]
-wui1 <- left_join(wui1, interface1, by=c("RegionalID", "pt_name", "pt_index"))
-
-## Add covariates
-subad1 <- subads %>% select(1:14) %>% distinct()
-
-# Join percent agriculture
-pctAG1 <- pctAG1[,c(1:3, 16)]
-subad1 <- left_join(subad1, pctAG1, by=c("RegionalID", "pt_name", "pt_index"))
-
-# Join beech basal area
-baa1 <- baa1[,c(1:3, 19)]
-subad1 <- left_join(subad1, baa1, by=c("RegionalID", "pt_name", "pt_index"))
-
-# Join total WUI
-wui1 <- wui1[,c(1:3, 26)]
-subad1 <- left_join(subad1, wui1, by=c("RegionalID", "pt_name", "pt_index"))
-
+wui1  <- dat %>% select(RegionalID, pt_name, pt_index, buffsize, radius, totalWUI) %>%
+  unite("buffrad", 4:5, sep="_") %>% 
+  group_by(RegionalID) %>% 
+  pivot_wider(names_from=buffrad, values_from=totalWUI) %>% 
+  as.data.frame()
+names(wui1)[4:12] <- c("wui_15_100", "wui_30_100", "wui_60_100",
+                       "wui_15_250", "wui_30_250", "wui_60_250",
+                       "wui_15_500", "wui_30_500", "wui_60_500") 
 #### With trace ####
 ## Loop over each set of random points ##
 m_est <- m_stderr <- pct2.5 <- pct97.5 <- m_ranef <- data.frame()
@@ -110,7 +83,9 @@ for (i in 1:10) {
   pt <- subad1 %>% filter(pt_index==i)
   
   # Run model with deltaAICc < 2
-  m1_pt <- clmm(catNcompT ~ Sex + pasture_30 + mix_30_100 + laggedBMI_30 + (1|WMU) + (1|year), data=pt, na.action="na.fail")
+  m1_pt <- glmmTMB(n.compounds.T ~ Sex + pasture_30 + mix_30_100 + 
+                      laggedBMI_30 + (1|WMU) + (1|year), data=pt, 
+                      family=compois(link = "log"), control=glmmTMBControl(parallel=nt))
   
   m1s <- summary(m1_pt)
   
