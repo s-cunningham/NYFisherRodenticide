a3834aa95f0d8f199862508fc40d213f94d6398d
Stephanie Cunningham <stcunnin@esf.edu>
2022-11-22 16:59:47 -0500

    Updating polygon layer to use

--- 05_spatial_layers.R
+++ 05_spatial_layers.R
@@ -18,7 +18,7 @@ loc <- loc[,-1]
 aea <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
 
 ## Read edited polygon layer back in
-twmu <- st_read("data/spatial", "ARerase_roadbuff")
+twmu <- st_read("data/spatial", "ARpolygons202211")
 st_crs(twmu)
 # Make sure projection is what it's supposed to be, and as a proj4 string
 twmu <- st_transform(twmu, aea)
