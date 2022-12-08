3359b58b8dc126536cdae1249448237a470f0c2d
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2022-12-08 13:55:10 -0300

    small edits

--- 06_combining_covariates.R
+++ 06_combining_covariates.R
@@ -26,7 +26,7 @@ pts <- read_csv("output/random_point_locs.csv")
 wmua <- read_csv("data/analysis-ready/wmuas.csv")
 lsm <- read_csv("data/analysis-ready/forest_lsm.csv")
 build <- read_csv("data/analysis-ready/building-centroid_sum.csv") %>%
-            rename(pt_name=name)
+            rename(pt_name=name) 
 
 #### Combine data ####
 ## Number of compounds detected
