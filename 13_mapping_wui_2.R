d47471ab396a5bf227cca027de4eddd80b2346ff
Stephanie Cunningham <stcunnin@esf.edu>
2023-04-17 12:11:54 -0400

    Updated figure 2 for manuscript

--- 13_mapping_wui_2.R
+++ 13_mapping_wui_2.R
@@ -38,37 +38,31 @@ tile2 <-  get_tiles(p2, provider="Esri.WorldImagery", zoom=11, cachedir=".")
 mix <- ggplot() +
   geom_spatraster_rgb(data=tile1, maxcell=Inf) +
   geom_spatraster_rgb(data=tile2, maxcell=Inf) +
-  geom_spatraster(data=intermix, maxcell=1000000, alpha=0.8) +
-  scale_fill_gradient(low="gray60", high="gray60", na.value=NA) +
-  geom_sf(data=roads) +
-  geom_sf(data=build, shape=20, size=0.5) +
+  geom_spatraster(data=intermix, maxcell=1000000, alpha=0.7) +
+  scale_fill_gradient(low="gray70", high="gray70", na.value=NA) +
+  geom_sf(data=roads, color="gray90") +
+  geom_sf(data=build, shape=20, size=0.5, color="gray90") +
   coord_sf(xlim=c(-75.49316986833249, -75.24503389115974), ylim=c(43.72828172346491, 43.818319582625065)) +
   theme_void() +
+  ggtitle("Intermix") +
   theme(legend.position="none")
 
 face <- ggplot() +
   geom_spatraster_rgb(data=tile1, maxcell=Inf) +
   geom_spatraster_rgb(data=tile2, maxcell=Inf) +
-  geom_spatraster(data=interface, maxcell=1000000, alpha=0.8) +
-  scale_fill_gradient(low="yellow", high="yellow", na.value=NA) +
-  geom_sf(data=roads, color="gray75") +
-  geom_sf(data=build, shape=20, size=0.5, color="gray75") +
+  geom_spatraster(data=interface, maxcell=1000000, alpha=0.7) +
+  scale_fill_gradient(low="gray70", high="gray70", na.value=NA) +
+  geom_sf(data=roads, color="gray90") +
+  geom_sf(data=build, shape=20, size=0.5, color="gray90") +
   coord_sf(xlim=c(-75.49316986833249, -75.24503389115974), ylim=c(43.72828172346491, 43.818319582625065)) +
   theme_void() +
+  ggtitle("Interface") +
   theme(legend.position="none")
 
-# not <- ggplot() +
-#   geom_spatraster_rgb(data=tile1, maxcell=Inf) +
-#   geom_spatraster_rgb(data=tile2, maxcell=Inf) +
-#   geom_spatraster(data=notwui, maxcell=1000000, alpha=0.8) +
-#   scale_fill_gradient(low="gray60", high="gray60", na.value=NA) +
-#   geom_sf(data=roads) +
-#   geom_sf(data=build, shape=20, size=0.5) +
-#   coord_sf(xlim=c(-75.49316986833249, -75.24503389115974), ylim=c(43.72828172346491, 43.818319582625065)) +
-#   theme_void() +
-#   theme(legend.position="none")
+face / mix 
 
-face / mix + plot_annotation(tag_levels="a", tag_prefix="(", tag_suffix=")")
+
+# plot_annotation(tag_levels="a", tag_prefix="(", tag_suffix=")")
 
   # geom_spatraster(data=wui, alpha=0.5, maxcell=2000000) +
   # scale_fill_discrete(na.value=NA) +
